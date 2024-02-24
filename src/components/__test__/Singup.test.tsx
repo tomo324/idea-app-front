c2cc2c8de6022b718a4576f632541ad053c97ebe
tomo324 <0204tomo.mc@gmail.com>
2024-02-24 17:32:24 +0900

    ✅ fetchのレスポンスが失敗の場合のテストを追加 #7

--- src/components/__test__/Singup.test.tsx
+++ src/components/__test__/Singup.test.tsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, RenderResult, waitFor } from "@testing-library/react";
 import Signup from "../Signup/Signup";
 
 describe("Signupコンポーネント", () => {
-  // テスト終了時fetch関数のモック化を解除
-  afterAll(() => {
-    (global.fetch as jest.Mock).mockClear();
+  let getByLabelText: RenderResult['getByLabelText'];
+  let getByRole: RenderResult['getByRole'];
+
+  // フォーム入力
+  const fillForm = () => {
+    fireEvent.change(getByLabelText("Email"), {
+      target: { value: "test@example.com" },
+    });
+    fireEvent.change(getByLabelText("Password"), {
+      target: { value: "password123" },
+    });
+    fireEvent.change(getByLabelText("Name"), { target: { value: "testUser" } });
+  };
+
+  // フォーム送信
+  const submitForm = async () => {
+    fireEvent.click(getByRole("button", { name: "Sign Up" }));
+    await new Promise(resolve => setTimeout(resolve, 100))
+  };
+
+  // テスト開始時にSignupコンポーネントをレンダリング
+  beforeEach(() => {
+    const renderResult = render(<Signup />);
+    getByLabelText = renderResult.getByLabelText;
+    getByRole = renderResult.getByRole;
   });
 
-  test("フォーム入力が正常に行え、fetchが正しく呼び出されていること", async () => {
+  // テスト終了時fetch関数のモック化を解除
+  afterEach(() => {
+    (global.fetch as jest.Mock).mockClear();
+    jest.restoreAllMocks();
+  });
+
+  it("フォーム入力が正常に行え、fetchが正しく呼び出されていること", async () => {
     const mockResponse = {
       ok: true,
       status: 200,
@@ -21,23 +49,12 @@ describe("Signupコンポーネント", () => {
 
     // fetch関数をモック化
     global.fetch = jest.fn().mockImplementation(dataSignupMock);
+    
+    // フォームを埋め送信
+    fillForm();
+    await submitForm();
 
-    // Signupコンポーネントをレンダリング
-    const { getByLabelText, getByRole } = render(<Signup />);
-
-    // 入力フィールドに値を入力
-    fireEvent.change(getByLabelText("Email"), {
-      target: { value: "test@example.com" },
-    });
-    fireEvent.change(getByLabelText("Password"), {
-      target: { value: "password123" },
-    });
-    fireEvent.change(getByLabelText("Name"), { target: { value: "testUser" } });
-
-    // 送信ボタンをクリック;
-    fireEvent.click(getByRole("button", { name: "Sign Up" }));
-
-    // fetchが一度だけ呼び出されたことを確認
+    // fetchが一度だけ呼び出されたことをテスト
     expect(global.fetch).toHaveBeenCalledTimes(1);
 
     // fetchの引数が正しいことをテスト
@@ -58,6 +75,44 @@ describe("Signupコンポーネント", () => {
     expect(fetchResponse).toEqual(mockResponse);
   });
 
-  // TODO エラーになる場合の、コンソールの出力ををテストする 
+  it("fetchのレスポンスが失敗した場合、console.logとalertが実行されること", async () => {
+
+    const mockResponse = {
+      ok: false,
+      status: 401,
+      json: async () => ({
+        error: "Unauthorized",
+      }),
+    };
+
+    const dataSignupMock = () => Promise.resolve(mockResponse);
+
+    // fetch関数をモック化
+    global.fetch = jest.fn().mockImplementation(dataSignupMock);
+
+    // console.logをモック化
+    jest.spyOn(console, 'log');
+
+    // alertをモック化
+    jest.spyOn(window, 'alert');
+
+    // フォームを埋めて送信
+    fillForm();
+    await submitForm();
+
+    // fetchが一度だけ呼び出されたことを確認
+    expect(global.fetch).toHaveBeenCalledTimes(1);
+
+    // console.logが一度だけ呼び出されたことを確認
+    expect(console.log).toHaveBeenCalledTimes(1);
+    // console.logの引数が正しいことを確認
+    expect(console.log).toHaveBeenCalledWith('Server Error', { error: 'Unauthorized' });
+
+    // alertが一度だけ呼び出されたことを確認
+    expect(window.alert).toHaveBeenCalledTimes(1);
+    // alertの引数が正しいことを確認
+    expect(window.alert).toHaveBeenCalledWith('Server Error');
+  });
+
   // TODO バリデーション用のテストを追加する
 });
