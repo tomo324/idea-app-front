ac086df05e67274613ef7138eeaade1ccb90948f
tomo324 <0204tomo.mc@gmail.com>
2024-02-26 14:42:11 +0900

    🦺 #7 バリデーションを追加

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -3,14 +3,15 @@ import { z } from "zod";
 export const signupValidationSchema = z.object({
   email: z
     .string()
-    .min(1, "email is required")
-    .email("invalid email"),
+    .min(1, "メールアドレスは必須です")
+    .max(255, "メールアドレスは255文字以内で入力してください")
+    .email("メールアドレスの形式が正しくありません"),
   name: z
     .string()
-    .min(1, "name is required")
-    .max(20, "name must be less than 20 characters"),
+    .min(1, "名前は必須です")
+    .max(20, "名前は20文字以内で入力してください"),
   password: z
     .string()
-    .min(6, "password must be at least 6 characters")
-    .max(16, "password must be less than 16 characters"),
+    .min(6, "パスワードは6文字以上で入力してください")
+    .max(127, "パスワードは127文字以内で入力してください"),
 });
