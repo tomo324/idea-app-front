55b014d27110d603c6970513058c24f7ec7107e9
tomo324 <0204tomo.mc@gmail.com>
2024-04-03 17:42:48 +0900

    💄 #3 サイドバーに現在地を表示

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,22 @@
-'use client';
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { useFetchUser } from "../../hooks/useFetchUser";
+import Sidebar from "@/components/Menu/Sidebar/Sidebar";
 
 const UserHome = () => {
-
   // ユーザー情報を取得するカスタムフックを使う
   const { data, error, isLoading } = useFetchUser();
+
+  //const data = { name: "test" };
+  //const isLoading = false;
 
   if (error) return <div>Error: {error.message}</div>;
   if (isLoading) return <div>loading...</div>;
 
   return (
     <div>
-      <h1>hello {data.name}!</h1>
+      <Sidebar />
     </div>
   );
 };
--- src/components/Menu/Sidebar/Sidebar.tsx
+++ src/components/Menu/Sidebar/Sidebar.tsx
@@ -1,21 +1,32 @@
-import React from 'react';
+"use client";
+
+import React from "react";
 import Link from "next/link";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
+import { usePathname } from "next/navigation";
 
 const Sidebar: React.FC = () => {
   const icons = [
-    { icon: 'material-symbols:home-outline', path: '/home' },
-    { icon: 'mdi:post-outline', path: '/posts' },
-    { icon: 'carbon:ai', path: '/ai-posts' },
-    { icon: 'mdi:user-outline', path: '/users' },
+    { icon: "material-symbols:home-outline", path: "/home" },
+    { icon: "mdi:post-outline", path: "/posts" },
+    { icon: "carbon:ai", path: "/ai-posts" },
+    { icon: "mdi:user-outline", path: "/users" },
   ];
 
+  const pathname = usePathname();
+
   return (
-    <div className='w-20 h-screen bg-white'>
+    <div className="w-20 h-screen bg-white">
       <div className="flex flex-col justify-between items-center h-3/5 w-20 bg-white">
         {icons.map(({ icon, path }) => (
-          <Link href={path} key={path}>
-            <Icon icon={icon} className="my-4 text-2xl" />
+          <Link
+            href={path}
+            key={path}
+            className={`p-2 flex items-center my-4 ${
+              pathname === path ? "border-l-2 border-blue-500 h-8" : ""
+            }`}
+          >
+            <Icon icon={icon} className="text-2xl" />
           </Link>
         ))}
       </div>
