3e051ae99f82b6a1b9f32a67e9cf933038e0d40f
tomo324 <0204tomo.mc@gmail.com>
2024-05-16 20:49:33 +0900

    💄 #77 トップページを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,33 +8,33 @@ export default function Home() {
       <div className="absolute top-0 right-0 p-3 flex space-x-2">
         <Link
           href="/auth/signin"
-          className="font-medium py-1 px-2 sm:py-2 sm:px-4 lg:text-xl text-xs hover:text-[#81c0e9] cursor-pointer"
+          className="font-medium py-1 px-2 sm:py-2 sm:px-4 lg:text-xl text-xs hover:text-gray-500 cursor-pointer"
         >
           Login
         </Link>
         <AuthButton path="/auth/signup">Sign Up</AuthButton>
       </div>
-      <div className="flex h-4/6">
-        <div className="bg-[#a2dbfa] flex-1 flex items-center justify-center flex-col">
-          <div className="my-20 p-3">
+      <div className="flex h-full">
+        <div className="bg-[#7bc6ef] flex-1 flex items-center justify-center flex-col">
+          <div className="flex-1 flex items-center justify-center pt-20">
+            <h1 className="text-white md:text-7xl text-3xl text-center font-ibm">
+              AIdea Park
+            </h1>
+          </div>
+          <div className="my-5 p-3">
             <Image
               src="/home-idea.png"
               alt="idea-image"
-              width={300}
-              height={300}
+              width={400}
+              height={400}
               priority={true}
             />
-            <h1 className="mt-4 text-center text-white sm:text-xl text-sm">
+            <h1 className="mt-16 pb-20 text-center text-white sm:text-xl text-sm">
               あなたのアイデアに、
               <br />
               さらなる可能性を。
             </h1>
           </div>
-        </div>
-        <div className="flex-1 flex items-center justify-center">
-          <h1 className="text-[#a2dbfa] md:text-7xl text-3xl text-center font-ibm border-b-2 border-[#a2dbfa]">
-            AIdea Park
-          </h1>
         </div>
       </div>
       <div className="m-7">
@@ -44,24 +44,21 @@ export default function Home() {
           ユーザーによって共有されたアイデアはAIによって融合され、新しいアイデアを発見できます。
         </p>
       </div>
-      <div>
+      <div className="mt-28 flex justify-center">
         {/* アイデア融合の例をここに記述 */}
-        <p className="text-center">
-          aaaaaaaaaaaaaaaaaaa
-          <br />
-          aaaaaaaaaaaaaaaaaaaaaaaa
-          <br />
-          aaaaaaaaaaaaaaaaaaaaaaaaaaa
-          <br />
-          bbbbbbbbbbbbbbbbbbbbbbbbbbbb
-          <br />
-          bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-          <br />
-          ccccccccccccccccccccccccccccccccccccccc
-          <br />
-          ccccccccccccccccccccccccccccccccccccccccc
+        <p className="text-left">
+          アイデア融合の例
           <br />
         </p>
+      </div>
+      <div className="flex justify-center">
+        <Image
+          src="/idea-example.png"
+          alt="home-example"
+          width={600}
+          height={600}
+          priority={true}
+        />
       </div>
     </main>
   );
--- src/components/Auth/AuthButton/AuthButton.tsx
+++ src/components/Auth/AuthButton/AuthButton.tsx
@@ -8,7 +8,7 @@ interface AuthButtonProps {
 
 const AuthButton: React.FC<AuthButtonProps> = ({ path, children }) => {
   return (
-    <Link href={path} className="inline-block bg-[#a2dbfa] text-white font-medium py-1 px-2 sm:py-2 sm:px-4 lg:text-xl text-xs rounded hover:bg-[#81c0e9] cursor-pointer">
+    <Link href={path} className="inline-block bg-white text-[#7bc6ef] font-medium py-1 px-2 sm:py-2 sm:px-4 lg:text-xl text-xs rounded hover:bg-blue-100 cursor-pointer">
       {children}
     </Link>
   );
