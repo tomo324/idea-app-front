e3a47a755db495e526ca2a007dcaff36f61da690
tomo324 <0204tomo.mc@gmail.com>
2025-02-02 21:56:47 +0900

    chore: delete imports

--- src/app/ai-posts/related/page.tsx
+++ src/app/ai-posts/related/page.tsx
@@ -2,10 +2,6 @@
 
 import { useSearchParams } from "next/navigation";
 import RelatedAiPostList from "@/components/AiPost/RelatedAiPostList/RelatedAiPostList";
-import Header from "@/components/Header/Header";
-import { BrowserView, MobileView } from "react-device-detect";
-import Sidebar from "@/components/Menu/Sidebar/Sidebar";
-import Footer from "@/components/Menu/Footer/Footer";
 
 const AiPostRelatedPage = () => {
   const searchParams = useSearchParams();
